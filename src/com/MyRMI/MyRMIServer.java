50319fd387fef3bb81da49cacfcfb2d9a2c2599c
Denis <denis.selitsky@gmail.com>
2022-10-19 11:56:29 +0300

    Register the service with the RMI regestry

--- src/com/MyRMI/MyRMIServer.java
+++ src/com/MyRMI/MyRMIServer.java
@@ -7,6 +7,17 @@ import com.MyRMI.MyRMIClient.MyRemote;
 
 public class MyRMIServer {
     public static void main(String[] args) {
+        /*
+         *  Need to do available to remote clients. To do this by instantiating it and putting it into the RMI
+            registry (which must be running or this line of code fails). When register the implementation object, the RMI system actually puts the stub in
+            the registry, since that's what the client really needs. Register service using the static rebind() method of the java.rmi.Naming class
+         */
+        try {
+            MyRMIServer service = new MyRMIServer();
+            Naming.rebind("Remote Hello", service);
+        }catch(Exception ex) {
+            ex.printStackTrace();
+        }
         
     }
 
@@ -15,6 +26,11 @@ public class MyRMIServer {
             functionality related to 'being remote'. The simplest way is to extend
             UnicastRemoteObject 
         */
+        /*
+         * Need to declare a constructor for remote implementation,
+         * just so that have a place to declare the RemoteException
+         */
+        public MyRemoteImpl() throws RemoteException{};
         public String sayHello() {
             return "Server saying Hi";
         }
