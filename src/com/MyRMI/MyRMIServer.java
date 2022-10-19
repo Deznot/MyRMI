9679211117f2dd821899c6c5f04051e6aa456836
Denis <denis.selitsky@gmail.com>
2022-10-19 11:42:09 +0300

    feat: add comments for server and add constructor for MyRemoteImpl

--- src/com/MyRMI/MyRMIServer.java
+++ src/com/MyRMI/MyRMIServer.java
@@ -11,6 +11,10 @@ public class MyRMIServer {
     }
 
     public class MyRemoteImpl extends UnicastRemoteObject implements MyRemote {
+        /*In order to work as a remote service object, object needs some
+            functionality related to 'being remote'. The simplest way is to extend
+            UnicastRemoteObject 
+        */
         public String sayHello() {
             return "Server saying Hi";
         }
