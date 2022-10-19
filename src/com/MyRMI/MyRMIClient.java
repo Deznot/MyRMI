6dc88813f56b2e10f95c1a0381b052714674de8e
Denis <denis.selitsky@gmail.com>
2022-10-19 16:00:04 +0300

    feat: add method go for client

--- src/com/MyRMI/MyRMIClient.java
+++ src/com/MyRMI/MyRMIClient.java
@@ -4,8 +4,21 @@ import java.rmi.*;
 
 public class MyRMIClient {
     public static void main(String[] args) {
-        
+        new MyRMIClient().go();
     }
+
+    public void go() {
+        try {
+            MyRemote service = (MyRemote) Naming.lookup("rmi://127.0.0.1/Remote Hello");
+
+            String s = service.sayHello();
+    
+            System.out.println(s);
+        }catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
     public interface MyRemote extends Remote {
         /*remote - interface-marker it has'nt methods 
          * the interface must announce that it's for remote method calls. An interface can't implement anything
