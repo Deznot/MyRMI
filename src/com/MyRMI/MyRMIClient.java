3e568d0e51e80e8402edb7ba56dcaebe5df36599
Denis <denis.selitsky@gmail.com>
2022-10-19 11:41:03 +0300

    feat: add comments for client

--- src/com/MyRMI/MyRMIClient.java
+++ src/com/MyRMI/MyRMIClient.java
@@ -7,6 +7,10 @@ public class MyRMIClient {
         
     }
     public interface MyRemote extends Remote {
+        /*remote - interface-marker it has'nt methods 
+         * the interface must announce that it's for remote method calls. An interface can't implement anything
+         * but it can extend other interface
+        */
         public String sayHello() throws RemoteException;
     }
    
